028ae4cb4743a108eb31992544c7b9e20a9e83fe
| X _ X | <104470074+HenryFacens@users.noreply.github.com>
2022-05-25 10:21:01 -0300

    Add files via upload

--- Trabalho/cryptograpy.c
+++ Trabalho/cryptograpy.c
@@ -4,81 +4,99 @@
 
 #define TEMP_FILE_NAME "FACENS PROJECT"
 
+
+// Funcao simples para criar uma copia do arquivo
+
 bool CopyFile(char INPUT_FILE_NAME[255], char OUTPUT_FILE_NAME[255]) {
 
-    FILE *input_file, *output_file;
+    FILE *input_file, *output_file;		//definindo o arquivo de entrada e saida "input_file ==> output_file" 
     char character;
 
-    input_file = fopen(INPUT_FILE_NAME, "r");
-    output_file = fopen(OUTPUT_FILE_NAME, "w");
-    if ((input_file == NULL) || (output_file == NULL)) {
-        return false;
+    input_file = fopen(INPUT_FILE_NAME, "r");	// Abrindo o arquivo no modo de leitura apenas para copiar os dados.
+    output_file = fopen(OUTPUT_FILE_NAME, "w");	// Abrindo o modo de reescrever o arquivo de entrada "input_file"
+    if ((input_file == NULL) || (output_file == NULL)) {	 // Verificando os arquivos se foram criados de forma bem sucedida.
+        return false;	// Caso os arquivos nao sejam criados ou acessados de forma correta, cancelar funcao.
     }
 
-    character = fgetc(input_file);
-    while (character != EOF) {   //EOF=erro
-		fputc(character, output_file);
-        character = fgetc(input_file);
+    character = fgetc(input_file); //pegando o primeiro caractere do documento de entrada.
+    
+    while (character != EOF) {   	// EOF=erro ==> executar apenas enquanto tiver caracteres restantes.
+		fputc(character, output_file); 	// adicionando o caractere copiado do arquivo de entrada para o arquivo de saida.
+        character = fgetc(input_file);		// pegando o proximo caractere do documento de entrada.
     }
 
-    fclose(input_file);
-    fclose(output_file);
+    fclose(input_file); // fechando o arquivo de entrada
+    fclose(output_file); // fechando o arquivo de saida 
 }
 
 
 bool encryption(char key[], char INPUT_FILE_NAME[], int method){
 
-    FILE *original_file, *temp_file;
+    FILE *original_file, *temp_file; //Arquivo original a se codificado/decodificado ih arquivo temporario para armazenar codificacao. 
     char character;
 
     //CopyFile(INPUT_FILE_NAME, "backup.txt");
-
-    original_file = fopen(INPUT_FILE_NAME, "r");
-    temp_file = fopen(TEMP_FILE_NAME, "w");
-    if ((original_file == NULL) || (temp_file == NULL)) {
-        return 0;
+	
+    original_file = fopen(INPUT_FILE_NAME, "r"); //Arquivo original sendo lido
+    temp_file = fopen(TEMP_FILE_NAME, "w"); //Arquivo temporario sendo escrito
+    if ((original_file == NULL) || (temp_file == NULL)) {// Verificando os arquivos se foram criados de forma bem sucedida.
+        return 0; // Caso os arquivos nao sejam criados ou acessados de forma correta, cancelar funcao.
     }
     
-    character = fgetc(original_file);
-    int position_key = 0;
-    while (character != EOF) {
-    	
-		character += key[position_key]*method;
+    character = fgetc(original_file); //pegando o primeiro caractere do documento de entrada.
+    int position_key = 0; // index da posicao de cada caractere da senha, comecando em 0.
+    while (character != EOF) { // EOF=erro ==> executar apenas enquanto tiver caracteres restantes.
+    
+		/*
+	Aqui pegamos a posicao na tabela ASCII de acordo com cada letra da senha.
+	Adicionado a posicao do char da senha, no conteudo do arquivo, criando um ciclo a seguir:
+
+	Senha123Senha123Senha123Senha123S
+	Esse seria o conteudo do arquivo.
+
+	Na primeira linha temos S da senha e E do conteudo, portando:
+	S = 83
+	E = 69
+	83+69 = 152
+	152 = '\x98'
+*/
+
+		character += key[position_key]*method; // Movimenta o char de acordo com o valor adotado na variavel "position_key" , assim movimentando o char na tabela ASCII.
 		
-        fputc(character, temp_file);
+        fputc(character, temp_file); // Copia as caracteres do arquivo temporario uma por uma.
         
-        if (key[position_key] == '\0') {
+        if (key[position_key] == '\0') { // Caso chegue no fim da senha recomecar leitura da senha, pelo index 0
             position_key = 0;
             
         }
-        else {
+        else { // Continuando leitura da senha.
             position_key += 1;
             
         }
         
-        character = fgetc(original_file);
+        character = fgetc(original_file); // pegando o proximo caractere do documento de entrada.
         
     }
 
-    fclose(original_file);
+    fclose(original_file); // fecha o arquivo original
     
-    fclose(temp_file);
+    fclose(temp_file); // fecha o arquivo temporario
 	
-    CopyFile(TEMP_FILE_NAME, INPUT_FILE_NAME);
+    CopyFile(TEMP_FILE_NAME, INPUT_FILE_NAME); //Copia arquivo temporario codificado para o arquivo original 
 
-    if (!remove(TEMP_FILE_NAME)) {
-        return true;
+    if (!remove(TEMP_FILE_NAME)) { //exclue o arquivo temporario criado 
+        return true; // Arquivo temporario exluido
     }
     else {
-        return false;
+        return false; //funcionou o codigo mas ele nao removeu o arquivo temporario. 
     }
 }
 
 
-bool Encode(char key[], char INPUT_FILE_NAME[]) {
+bool Encode(char key[], char INPUT_FILE_NAME[]) { // o Bool apenas troca o valor do METHOD, assim movimentando a tabela de acordo com a position_key utilizada.
     return encryption(key, INPUT_FILE_NAME, 1);
 }
 
-bool Decode(char key[], char INPUT_FILE_NAME[]) {
+bool Decode(char key[], char INPUT_FILE_NAME[]) { // o Bool apenas troca o valor do METHOD, assim movimentando a tabela de acordo com a position_key utilizada.
     return encryption(key, INPUT_FILE_NAME, -1);
 }
--- Trabalho/main.c
+++ Trabalho/main.c
@@ -1,74 +1,80 @@
 #include"main.h"
 
-int main()
-{inicio:
-setlocale(LC_ALL, "Portuguese");
-	char FileName[255], Key[255],resposta;
-int i;
-	printf("\n\t-----------------------------------------------------------------");
-	printf("\n\t|Voce deseja criptografar ou descriptografar ? Responda C ou D.\t|\t\n");
-	printf("\t-----------------------------------------------------------------");
-	printf("\n\n\t\tresposta:");
-	gets(&resposta);
-	tolower(resposta);
-if(resposta !='c'&& resposta !='d')
-{ 
 
-printf("\n\t\tNao e um caracter valido, coloque apenas C ou D !!!!!!!!!\n\n");
-goto inicio;
+int main() {
 
-}
-else if(resposta=='c')
-{
+  setlocale(LC_ALL, "Portuguese");
+  bool continuar = true;
 
+  while (true) { // repeticao.
+    char FileName[255],
+      Key[255],
+      resposta;
+    int i;
+    printf("\n\t-----------------------------------------------------------------");
+    printf("\n\t|Voce deseja criptografar ou descriptografar ? Responda C ou D.\t|\t\n");
+    printf("\t-----------------------------------------------------------------");
+    printf("\n\n\t\tresposta:");
+    gets( & resposta); //pega de resposta do usuario a pergunta feita no printf a cima 
+    resposta = tolower(resposta);
 
-	printf("\n\t\tDigite o nome do arquivo com o formato .txt como {NOME_DO_ARUIVO.txt} \n\n\t\t Nome do Arquivo : "); //digitar o arquivo .txt
-		gets(FileName);
-	printf("\n\t\tDigite a senha: ");
-		gets(Key);
-	Encode(Key, FileName);
-	printf("\n\t\t---------------------------");
-	printf("\n\t\t| Programa criptografado. |\n");
-		printf("\t\t---------------------------\n\n");
-	printf("\n\t\tPressione qualquer tecla para continuar...");
+    if (resposta != 'c' && resposta != 'd') { // confere se o usuario digitou corretamente.
+
+      printf("\n\t\tNao e um caracter valido, coloque apenas C ou D !!!!!!!!!\n\n");
+
+      continue;
+
+    } else if (resposta == 'c') {
+
+      printf("\n\t\tDigite o nome do arquivo com o formato .txt como {NOME_DO_ARUIVO.txt} \n\n\t\t Nome do Arquivo : "); //digitar o arquivo .txt
+      gets(FileName); // pega o nome do arquivo que deve estar contido no mesmo local do programa.
+      printf("\n\t\tDigite a senha: ");
+      gets(Key); // pega o nivel que voce queira movimentar ASCII.
+      Encode(Key, FileName);
+      printf("\n\t\t---------------------------");
+      printf("\n\t\t| Programa criptografado. |\n");
+      printf("\t\t---------------------------\n\n");
+      printf("\n\t\tPressione qualquer tecla para continuar...");
+      getchar();
+    } else if (resposta == 'd') {
+      printf("\n\t\tDigite o nome do arquivo com o formato .txt como {NOME_DO_ARUIVO.txt} \n\n\t\n\t\tDigite o nome do Arquivo: ");
+      gets(FileName);
+      printf("\n\t\tsenha do arquivo :");
+      gets(Key);
+      Decode(Key, FileName);
+
+      printf("\n\t\t---------------------------");
+      printf("\n\t\t| Programa desptografado. |\n");
+      printf("\t\t---------------------------\n\n");
+      
 	getchar();
-}
-	else if(resposta=='d'){
-	printf("\n\t\tDigite o nome do arquivo com o formato .txt como {NOME_DO_ARUIVO.txt} \n\n\t\n\t\tDigite o nome do Arquivo: ");
-		gets(FileName);
-	Decode(Key, FileName);
-	printf("\n\n\tsenha do arquivo :");
-		gets(Key);
-	printf("\n\t\t---------------------------");
-	printf("\n\t\t| Programa desptografado. |\n");
-		printf("\t\t---------------------------\n\n");
+	if(getchar() == 'egg'){}
 
+    while (true) {
 
-}
+      printf("\n\t\tDeseja continuar ? S ou N: ");
+      gets( & resposta);
+      resposta = tolower(resposta);
 
-yesorno:
-	printf("\n\t\tDeseja continuar ? S ou N: ");
-	gets(&resposta);
-	tolower(resposta);
-	
-	if(resposta!='s'&& resposta!='n'){
-		
-		printf("\n\t\tDigite apenas S ou N !!!!!!!!!\n\n");
-		goto yesorno;
-	}
-	
-	
-	else if(resposta=='s'){
-		goto inicio;
-	
-	}
-	else{
-		printf("\n\t\t-------------------------");
-	printf("\n\t\t|Programa Finalizado !!!|");
-	printf("\n\t\t-------------------------\n\n");
+      if (resposta == 's') {
+
+        break;
+      } else if (resposta == 'n') {
+
+        printf("\n\t\t-------------------------");
+        printf("\n\t\t|Programa Finalizado !!!|");
+        printf("\n\t\t-------------------------\n\n");
+        continuar = false;
+        break;
+      } else {
+        printf("\n\t\tDigite apenas S ou N !!!!!!!!!\n\n");
+      }
+    }
+    if (resposta == 'n') {
+      break;
+    }
+  }
+
 }
 }
-
-
-
 
